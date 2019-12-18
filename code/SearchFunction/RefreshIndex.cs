29aeb3c00889a32f129ad67bdfcb502bd3894620
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2019-12-18 13:58:53 -0500

    Fix CRON expression

--- code/SearchFunction/RefreshIndex.cs
+++ code/SearchFunction/RefreshIndex.cs
@@ -8,7 +8,7 @@ namespace SearchFunction
     public static class RefreshIndex
     {
         [FunctionName("refresh-search-index")]
-        public static void Run([TimerTrigger("0 0 12 ? * FRI *")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 0 0 * * FRI")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"Load at: {DateTime.Now}");
         }
